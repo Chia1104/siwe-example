b20a6657e2835d62af8f0719091ae607a0ac6410
Chia1104 <yuyuchia7423@gmail.com>
2025-02-19 12:07:43 +0800

    feat: init

---
+++ go.mod
@@ -0,0 +1,26 @@
+module siwe-example
+
+go 1.22.5
+
+require (
+	github.com/bits-and-blooms/bitset v1.17.0 // indirect
+	github.com/consensys/bavard v0.1.22 // indirect
+	github.com/consensys/gnark-crypto v0.14.0 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.1.0 // indirect
+	github.com/dchest/uniuri v1.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/ethereum/go-ethereum v1.15.2 // indirect
+	github.com/ethereum/go-verkle v0.2.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/relvacode/iso8601 v1.1.1-0.20210511065120-b30b151cc433 // indirect
+	github.com/spruceid/siwe-go v0.2.1 // indirect
+	github.com/supranational/blst v0.3.14 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,107 @@
+// This is a simple example of how to implement a web3 login endpoint using the siwe-go library.
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/spruceid/siwe-go"
+)
+
+const port = ":8080"
+
+func main() {
+	http.HandleFunc("/api/v1/auth/web3:login", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		err := Web3Login(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+// verifyWalletSignature verifies the signature of a wallet address
+func verifyWalletSignature(messageStr string, sig string) (jwt.MapClaims, error) {
+
+	message, err := siwe.ParseMessage(messageStr)
+	if err != nil {
+		err = fmt.Errorf("parse message err: %v", err)
+		return nil, err
+	}
+
+	verify, err := message.ValidNow()
+	if err != nil {
+		err = fmt.Errorf("verify message err: %v", err)
+		return nil, err
+	}
+
+	if !verify {
+		err = fmt.Errorf("verify message fail: %v", err)
+		return nil, err
+	}
+
+	publicKey, err := message.VerifyEIP191(sig)
+
+	if err != nil {
+		err = fmt.Errorf("verifyEIP191 err: %v", err)
+		return nil, err
+	}
+
+	pubBytes := crypto.FromECDSAPub(publicKey)
+	publicKeyString := hexutil.Encode(pubBytes)
+
+	// Return the verified claims
+	claims := jwt.MapClaims{
+		"web3_pub_key": publicKeyString,
+	}
+	return claims, nil
+}
+
+// LoginRequest represents the login request body
+type LoginRequest struct {
+	Message   string `json:"message"`
+	Signature string `json:"signature"`
+}
+
+func Web3Login(w http.ResponseWriter, r *http.Request) error {
+	// Parse the login request body
+	req := new(LoginRequest)
+
+	// Verify the signature
+	claims, err := verifyWalletSignature(req.Message, req.Signature)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	// Generate a JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return errors.New("error creating JWT token")
+	}
+
+	// Set the JWT token in the response header
+	http.SetCookie(w, &http.Cookie{
+		Name:    "jwt_token",
+		Value:   tokenString,
+		Expires: time.Now().Add(time.Hour * 24),
+	})
+
+	// Return a success response
+	w.WriteHeader(http.StatusOK)
+	return nil
+}
